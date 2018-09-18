c6ce8fc28124a8f5a66dd7e20f7ce16b2ea21aa8
Ziken <34137772+ZikenY@users.noreply.github.com>
2018-09-17 21:05:58 -0500

    Update contour_test.py

--- contour_test.py
+++ contour_test.py
@@ -1,28 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 定义等高线高度函数
+# generate the elevation from (x, y)
 def f(x, y):
     return (1 - x / 2 + x ** 5 + y ** 3) * np.exp(- x ** 2 - y ** 2)
 
-# 定义x, y向量
+# generate the grid
 n = 256
 x = np.linspace(-3, 3, n)
 y = np.linspace(-3, 3, n)
-
-# 生成网格数据
 X, Y = np.meshgrid(x, y)
 print('X.shape: ', X.shape)
 
-# 填充等高线的颜色, 8是等高线分为几部分
+# fill the color for the contour
 # plt.contourf(X, Y, f(X, Y), 8, alpha = 0.75, cmap = plt.cm.hot)
 
-# 绘制等高线
+# draw the contour
 C = plt.contour(X, Y, f(X, Y), 8, colors = 'green', linewidth = 0.5)
-# 绘制等高线数据
 plt.clabel(C, inline = True, fontsize = 10)
 
-# 去除坐标轴
+# get rid of the axis-lines
 plt.xticks(())
 plt.yticks(())
 plt.show()
